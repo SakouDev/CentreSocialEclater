7e4c69b8294d024403aa44d8cbe274550952fcf6
Uverlink <marc.pizzanelli@gmail.com>
2022-10-13 16:47:34 +0200

    Restructuration 2

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import cors from 'cors';
 
 const app = express();
 const routes = require('./routes.ts')
+const API_PORT = 5000
 
 app.use(express);
 app.use(cors());
+app.get("/", (req, res) => { res.send("Connexion Établie") });
+app.use('/api/utilisateur', routes);
+app.use('/api/auth', routes);
+app.listen(API_PORT, () => console.log('Initialisation du BACK-END terminée.'));
