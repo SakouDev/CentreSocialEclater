0a51714aac93b4d14e2e6095d6e15ff28c9a5b62
Sakou <86302989+SakouDev@users.noreply.github.com>
2022-10-19 15:59:03 +0200

    bcrypt on update too

--- routes/Forms/formEmployeurUpdate.ts
+++ routes/Forms/formEmployeurUpdate.ts
@@ -5,6 +5,7 @@ import { diplome } from "../../types/diplome";
 import { disponibilite } from "../../types/disponibilite";
 import { ApiException } from "../../types/exception";
 
+const bcrypt = require('bcrypt')
 const { Employeur, User, Disponibilite, UserDispo, Diplome } = require("../../database/connect");
 
 /**
@@ -38,7 +39,8 @@ const { Employeur, User, Disponibilite, UserDispo, Diplome } = require("../../da
   *          description: La requête s'est bien déroulé
   */
 module.exports = (app: Application) => {
-    app.put("/api/form/employeur/:id", (req, res) => {
+    app.put("/api/form/employeur/:id", async (req, res) => {
+        req.body.User.password = await bcrypt.hash(req.body.User.password, 10)
       
         Employeur.update(req.body.Employeur, {where : {id : req.params.id}}).then ((employeurmenfou : any) => {        
             Employeur.findByPk(req.params.id).then((employeur: employeur) => {
