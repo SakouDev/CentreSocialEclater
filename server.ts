9554e334a9d7d6ad421f38237d6c0907c3880cf3
Uverlink <marc.pizzanelli@gmail.com>
2022-11-09 10:44:48 +0100

    Correctif Database

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ const sequelize = require('./database/connect')
 const app = express()
 app.use(cors())
 app.use(express.json())
-// sequelize.initDb()
+sequelize.initDb()
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
