757ec0ea8fcf4ebd8785327aa5fbc7ec6c85877e
Uverlink <marc.pizzanelli@gmail.com>
2022-11-09 15:17:51 +0100

    Correctif Merge 3

--- models/user-diplome.ts
+++
@@ -1,8 +0,0 @@
-import {  DataTypes, Sequelize, STRING } from "sequelize"
-
-
-module.exports = (sequelize : Sequelize, dataTypes : typeof DataTypes) => {
-    
-    return sequelize.define('UserDiplome', {
-    })
-}
